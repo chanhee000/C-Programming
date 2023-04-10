10ff7b3fe23d12b6489ee1a16dab42003337ccda
chanhee000 <125625470+chanhee000@users.noreply.github.com>
2023-04-10 22:26:58 +0900

    Add files via upload

---
+++ 20230410/20230410_2/20230410_2/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#define M_PI
+#include<math.h>
+
+
+int main()
+{
+	double sad, haha;
+	haha = 1;
+	sad = haha + M_PI;
+	printf("%f", sad);
+	return 0;
+
+
+
+
+}
