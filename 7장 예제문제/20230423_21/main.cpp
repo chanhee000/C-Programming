cfdfa3f6920d3fcaa235c9a30ca26a87cff98764
chanhee000 <125625470+chanhee000@users.noreply.github.com>
2023-04-23 21:51:37 +0900

    Add files via upload

---
+++ 7장 예제문제/20230423_21/main.cpp
@@ -0,0 +1,16 @@
+//291p¿¹Á¦2¹ø
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+	int x, y;
+	for (y = 1; y <= 5; y++)
+	{
+		for (x = 0; x < y; x++)
+			printf("*");
+
+		printf("\n");
+	}
+	return 0;
+}
