bbe0f96ae32b8f8e4e56bf43dbfa5c3f2db05faa
chanhee000 <125625470+chanhee000@users.noreply.github.com>
2023-04-23 21:53:13 +0900

    Add files via upload

---
+++ 7장 예제문제/20230423_23/main.cpp
@@ -0,0 +1,18 @@
+//29p º¹¸®
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define SEED_MONEY 1000000
+int main(void)
+{
+	int year = 0, money = SEED_MONEY;
+
+	while (1)
+	{
+		year++;
+		money = money * (1 + 0.30);
+		if (money > 10 * SEED_MONEY)
+			break;
+	}
+	printf("%d³â\n", year);
+	return 0;
+}
---
+++ 7장 예제문제/20230423_25/main.cpp
@@ -0,0 +1,15 @@
+//296p continue ¹Ýº¹¹®
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i % 3 == 0)
+			continue;
+		printf("%d", i);
+	}
+	return 0;
+}
