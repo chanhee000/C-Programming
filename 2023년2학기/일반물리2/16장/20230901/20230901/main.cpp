936c6dabe0b04f8cec79da41cf2d497d255304d5
chanhee000 <125625470+chanhee000@users.noreply.github.com>
2023-09-04 22:08:41 +0900

    Add files via upload

---
+++ 2023년2학기/일반물리2/16장/20230901/20230901/main.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+	double kc = 8.99 * pow(10, 9);
+	double q1 = 0;
+	double q2 = 0;
+	double r = 0;
+	double Fc = 0;
+
+	printf("Fc의 값을 구하세요\n");
+
+	printf("r,q1,q2의 값을 입력하세요:\n");
+	scanf("%lf,%lf,%lf", &r, &q1, &q2);
+
+	printf("Fc=%lf", kc * ((q1 * q2) / (r * r)));
+
+}
