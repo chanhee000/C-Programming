0710dbdaa4881289e651ed6ee40a44258c3d295b
chanhee000 <125625470+chanhee000@users.noreply.github.com>
2023-10-30 20:59:28 +0900

    Add files via upload

---
+++ 2023년2학기/물리/물리17장/저항값/Find resistance 2 value/Find resistance 2 value/main.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    double voltage = 5.0;
+    double resistance1 = 0.0;
+    double voltage_out = 0;
+
+    printf("저항1의 값을 입력하세요: ");
+    scanf("%lf", &resistance1);
+    printf("\n");
+
+
+    printf("(소수점 다 적어주셔야 정확히 나옵니다.)\n V_out 값 입력: ");
+    scanf("%lf", &voltage_out);
+    printf("\n");
+
+    double resistance2 = (resistance1 * voltage_out) / (voltage - voltage_out);
+    printf("저항2의 값은 %.2lf입니다. ", resistance2);
+
+    return 0;
+}
